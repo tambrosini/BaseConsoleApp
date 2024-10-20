4293601e73864fe9ccdb740821b97b24bd30e92a
Thomas Ambrosini <thomas.ambrosini@playtimesolutions.com.au>
2024-10-21 08:54:55 +1100

    Initial commit

---
+++ BaseConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace BaseConsoleApp;
+
+public class Program
+{
+    public const string ApplicationName = "Base Console App";
+    
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Starting {ApplicationName}...");
+
+        bool running = true;
+
+        while (running)
+        {
+            // Display a prompt
+            Console.Write("> ");
+            string input = Console.ReadLine();
+
+            switch (input.ToLower())
+            {
+                case "exit":
+                    running = false;
+                    Console.WriteLine("Exiting...");
+                    break;
+                default:
+                    Console.WriteLine($"Command '{input}' not recognised.");
+                    break;
+            }
+        }
+        
+        Console.WriteLine("Program closed.");
+    }
+}
